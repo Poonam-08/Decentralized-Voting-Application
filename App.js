b5e6021d3e4dec30836350cd30e74c61d08d4738
Poonam-08 <131946970+Poonam-08@users.noreply.github.com>
2024-10-06 14:20:07 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Voting from './Voting';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Decentralized Voting Application</h1>
+            <Voting />
+        </div>
+    );
+}
+
+export default App;
